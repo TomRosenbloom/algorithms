65431b5e3fd7d590a735de68d836bf8a1af7bce8
Tom <Tom@desktop-pc.lan>
2016-05-07 18:16:29 +0100

    basic counting sort, description updated

--- classics/countingSort.php
+++ classics/countingSort.php
@@ -12,6 +12,14 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
         <p>Create an array of counters (an array that shows the number of occurrences
         of each number between 0 and the largest in the array). Then just read through 
         that array to create the ordered output array.</p>
+        <p>Example: let's say the array to be sorted is [3,2,5,2]. The count array of this will 
+        be [0,0,2,1,0,1], meaning there are zero 0's, zero 1's, two 2's, one 3 and so on to 
+        the highest value found in the array. Each array key represents a value that might 
+        be found in A, and the corresponding value os the number of times it was found. 
+        To create the sorted array we iterate through the count array and for each 
+        key value pair we write 'key' 'value' number of times into the output array.</p>
+        <p>Note, this simple version of count sort is not 'stable' i.e. will not preserve
+            the order of similar values. See <a href="countingSortStable.php">countingSortStable</a> for the more complicated stable version</p>
         <p>This looks like it ought to have a time complexity of O(n^2)
         because of the nested loops, but is actually O(n+m) because the nesting 
         of those loops doesn't represent the product of every member of one 
@@ -38,7 +46,7 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
         function countingSort(array $A){
             
             $B = array();
-            $C = countingArray($A);
+            $C = countingArray($A); echo implode("",$C)."<br>";
             $n = count($C);
             
             foreach($C as $number=>$count){
