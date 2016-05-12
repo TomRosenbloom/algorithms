2588f9ce5668b512e9107a5c8eb5eebaf6796639
Tom <Tom@desktop-pc.lan>
2016-05-12 12:25:11 +0100

    frogRiverOne 100/100

--- frogRiverOne.php
+++ frogRiverOne.php
@@ -14,45 +14,44 @@ function solution($A,$X) {
     // then stop when the array is empty - but this will cause array index not found warnings
     // (plus you keep having to count the array)
     
-    // the below scores 83% and 80%
-    // fails on single element, and large_permutation
-    // I can fix for single element, but that might be as good as I can get 
-    // with this methodology, at least I'm not
-    // going to improve the space complexity
+    // the below gets 100/100
+    // I had to cheat and google the solution - I was on the right lines but the key
+    // mistake was using an array, when all it really needs is a decrementing counter
+    
+
     
     $n = count($A);
 
 
     $C = array_fill(1, $X, 0);
 
-    $k = 0;
-    for($i = 0; $i < $n-1; $i++){
-        echo implode(",", $C)."<br>";
-        echo count($C)."<br>";
-        echo $A[$i]."<br>";
-        if(isset($C[$A[$i]])){
-            unset($C[$A[$i]]);
-        }
-        echo count($C)."<br>";
-        if(count($C) == 0){
-            return $k;
-        }
-        $k++;
+    $uncovered = $X; // initially all are uncovered
+    
+    for($i = 0; $i < $n; $i++){
+        
+        if($C[$A[$i]] == 0){ // not yet covered
+            $C[$A[$i]]++; // so cover
+            $uncovered--; // decrement count of uncovered positions
+            if($uncovered == 0){ // if all have been covered
+                return $i;
+            }
+        } 
+
     }
-    if(count($C) > 0){
-        return -1;
-    }
+    
+    return -1;
+    
 } 
 
 
 //$A = array(1,3,1,4,2,3,5,4);
 //$X = 5;
 
-$A = array(1);
-$X = 1;
+//$A = array(1);
+//$X = 1;
 
-//$A = array(1,3,1,4,2,3,4,4);
-//$X = 5;
+$A = array(1,3,1,4,2,3,4,4);
+$X = 5;
 
 print_r(solution($A,$X));
 
