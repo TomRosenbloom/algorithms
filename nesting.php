254030a42060fad1df5a04573835c243073b02ef
Tom <Tom@desktop-pc.lan>
2016-06-07 14:21:03 +0100

    nesting 100/100 - the trick is you don't need a stack, just a counter is sufficient

--- nesting.php
+++ nesting.php
@@ -2,26 +2,10 @@
 
 function solution($S) {
     
-    // this should be easy
-    // go through the string
-    // if char = open bracket, put onto stack
-    // if char = closed bracket, pop the last char of the stack and compare:
-    // if it is an open bracket, carry on
-    // if it is a closed bracket (or null) then error
-    
-    // you might think this won't work because it is legit to have substrings of more than one closed bracket
-    // but this can't happen in the stack
-    
-    // hmm, this gets 100/75 fails on:
-    // broad_tree_with_deep_paths 
-    // string of the form (TTT...T) of 300 T's, each T being '(((...)))' nested 200-fold, length=1 million
-    
-    // I tried using a pointer, to get rid of count($stack) for each iteration
-    // but still same result
+    // the trick here is you don't actually need a stack
     
     
-    $stack = array();
-    $pointer = 0;
+    $openBrackets = 0;
     
     for($i = 0; $i < strlen($S); $i++){
         
@@ -30,25 +14,23 @@ function solution($S) {
         echo "<br>$char<br>";
         
         if($char == "("){
-            $pointer = array_push($stack, $char);
+            $openBrackets++;
         }  
         
-        echo $pointer . "<br>";
+        echo $openBrackets . "<br>";
         
         if($char == ")"){
-            if($pointer == 0){
+            if($openBrackets == 0){
                 return 0;
             } else {
-                array_pop($stack);
-                $pointer--;
+                $openBrackets--;
             }
         } 
         
-        echo implode("",$stack) . "<br>";        
         
     }
     
-    if(count($stack) > 0){
+    if($openBrackets > 0){
         return 0;
     } else {
         return 1;
@@ -57,7 +39,7 @@ function solution($S) {
 }
 
 $S = "(()(())())";
-$S = "())";
+//$S = "())";
 
 
 print_r(solution($S));
