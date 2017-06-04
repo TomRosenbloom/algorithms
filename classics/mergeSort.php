1a146b51085fe7bf4a143f1b763bdebca6599a38
Tom <Tom@desktop-pc.lan>
2017-06-04 23:00:37 +0100

    php merge sort, additional notes

--- classics/mergeSort.php
+++ classics/mergeSort.php
@@ -15,6 +15,8 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
             <input type="submit" value="Sort">
         </form>
         <?php
+        // 45,75,37,8,19,46,52,2,28,6,78,89,39,54,16,7,13,12,5,11,4
+        
         $A = array();
         
         if(isset($a)){
@@ -30,15 +32,23 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
         // method A
         function divide(array $arr) {
             
+            // (not the best name for the function because it doesn't just divide,
+            // it also calls the merge ('conquer') function)
+            
             // terminating condition for recursion: single element array
             if (1 === count($arr)) { 
                 return $arr;
             }
             
             // divide the current array into left and right half
+            // (what about odd/even numbers of array elements?
+            // if n=8 then $middle=4, left array elements numbered 0,1,2,3, right 4,5,6,7
+            // if n=9, $middle=5, left array numbered 0,1,2,3,4, right 5,6,7,8,9 
+            // so the right array has one more element and this simply means the recursion
+            // has to go one level deeper at the rightmost end of the array)
             $left = $right = array();
             $middle = round(count($arr)/2);
-            for ($i = 0; $i < $middle; ++$i) {
+            for ($i = 0; $i < $middle; ++$i) { // simply copying elements from the original array into the new subarrays
                 $left[] = $arr[$i];
             }
             for ($i = $middle; $i < count($arr); ++$i) {
@@ -49,8 +59,11 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
             // this does all the left hand side, then all the right hand side
             $left = divide($left);
             $right = divide($right);
+  
+            // so we end up at the end of the recursion with n 1-element arrays
+            // now we merge these, sorting as we go
             
-            echo "<br>We are going to conquer these two arrays: array(",
+            echo "<br><br>We are going to conquer these two arrays: array(",
             implode(", ", $left), ")\narray(", implode(", ", $right), ")";
             
             // when both recusions are complete, 
@@ -68,11 +81,15 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
                 if (count($left) > 0 && count($right) > 0) { // if both arrays have something in them
                     // order them...
                     // get the first element of each array
-                    $firstLeft = current($left); // current() is the first element in the array
+                    $firstLeft = current($left); // current() is the first element in the array (current() "returns the value of the array element that's currently being pointed to by the internal pointer"
                     $firstRight = current($right);
+                    echo "<br>first left $firstLeft, first right $firstRight";
                     // put them into result array in correct order
+                    // It's obvious that this works for single element subarrays, but how can it work for longer subarrays?
+                    // The answer is that once we are handling longer subarrays, each one is already sorted
+                    // - that's kind of the whole point!
                     if ($firstLeft <= $firstRight) {
-                        $result[] = array_shift($left); // array_shift removes and returns the first array element
+                        $result[] = array_shift($left); // array_shift removes and returns the first array element 
                     } else {
                         $result[] = array_shift($right);
                     }
@@ -83,6 +100,7 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
                     // put first element of right array into result array
                     $result[] = array_shift($right);
                 }
+                echo "<br>" . implode(", ", $result);
             }
             return $result;
             // so, when we first come into this routine we will be in the deepest 
@@ -93,7 +111,6 @@ $a = filter_input(INPUT_POST, 'array',FILTER_SANITIZE_STRING);
             // on the second iteration we just have the 3 remaining in the left array
             // so that is moved to the result array
             
-            // once we have dealt with 
         }   
 
 
